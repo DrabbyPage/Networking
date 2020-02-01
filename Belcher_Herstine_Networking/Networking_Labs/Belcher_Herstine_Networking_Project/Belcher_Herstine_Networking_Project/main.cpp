5de631acd1240986ba2a02386bbfd081d13d7dc0
Cameron Belcher <cameron.belcher@mymail.champlain.edi>
2020-02-01 13:19:42 -0500

    made a print screen for teh tic tac toe data and win condition check for tic tac toe battleship also has a print to screen but... I dont have a win condition

--- Belcher_Herstine_Networking/Networking_Labs/Belcher_Herstine_Networking_Project/Belcher_Herstine_Networking_Project/main.cpp
+++ Belcher_Herstine_Networking/Networking_Labs/Belcher_Herstine_Networking_Project/Belcher_Herstine_Networking_Project/main.cpp
@@ -122,16 +122,29 @@ struct Host
 };
 #pragma pack(pop)
 
-struct TicTacToeGameData
+struct TicTacToeFullGameData
 {
 	unsigned char typeId = ID_RECEIVE_TIC_TAC_TOE;
 	char gameData[3][3];
 };
 
-struct BattleShipGameData
+struct BattleShipFullGameData
 {
 	unsigned char typeId = ID_RECEIVE_BATTLESHIP;
 	char gameData[10][10];
+};
+
+struct BattleshipData
+{
+	unsigned char typeId = ID_RECEIVE_BATTLESHIP;
+	char yPos; // has to be a letter
+	char xPos; // has to be a Number
+};
+
+struct TicTacToeData
+{
+	unsigned char typeId = ID_RECEIVE_TIC_TAC_TOE;
+	char ticTacToePos; // would be a... number?
 };
 
 
@@ -870,4 +883,97 @@ void DoMyPacketHandlerHost(Host* hostPack)
 
 
 	// Perform the functionality for this type of packet, with your struct,  MyStruct *s
+}
+
+void PrintBattleshipGameData(BattleShipFullGameData bsData)
+{
+	// 1 2 3 4 5 6 7 8 (the top of the battle ship game
+	std::cout << " ";
+	for (int i = 0; i < 9; i++)
+	{
+		std::cout << i << " ";
+	}
+	std::cout << std::endl;
+	//
+
+	for (int i = 0, char side = 'a'; i < 9; i++, side++)
+	{
+		// a b c d e f g h (side) (the start of teh line that is)
+		std::cout << side << " ";
+
+		// orint the rest of the line data
+		for (int j = 0; j < 9; j++)
+		{
+			std::cout << bsData.gameData[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+void PrintTicTacToeGameData(TicTacToeFullGameData tttData)
+{
+	for (int i = 0; i < 3; i++)
+	{
+		std::cout << " ";
+		for (int j = 0; j < 3; j++)
+		{
+			std::cout << tttData.gameData[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+// will return X, O or... i think -52 when using this X is for p1 O for p2 -52 for no input
+char CheckTicTacToeWinCondition(TicTacToeFullGameData tttData)
+{
+	char winner;
+
+	// any of the rows is same
+	for (int i = 0; i <= 2; i++)
+	{
+		if (tttData.gameData[i][0] == tttData.gameData[i][1] && tttData.gameData[i][1] == tttData.gameData[i][2] && tttData.gameData[i][0] != 0)
+		{
+			winner = tttData.gameData[i][0];
+			return winner;
+		}
+	}
+
+	// any of the columns is same
+	for (int i = 0; i <= 2; i++)
+	{
+		if (tttData.gameData[0][i] == tttData.gameData[1][i] && tttData.gameData[1][i] == tttData.gameData[2][i] && tttData.gameData[0][i] != 0)
+		{
+			winner = tttData.gameData[0][i];
+			return winner;
+		}
+	}
+
+	// 1st diagonal is same
+	if (tttData.gameData[0][0] == tttData.gameData[1][1] && tttData.gameData[1][1] == tttData.gameData[2][2] && tttData.gameData[0][0] != 0)
+	{
+		winner = tttData.gameData[0][0];
+		return winner;
+	}
+
+	// 2nd diagonal is same
+	if (tttData.gameData[0][2] == tttData.gameData[1][1] && tttData.gameData[1][1] == tttData.gameData[2][0] && tttData.gameData[0][2] != 0)
+	{
+		winner = tttData.gameData[0][2];
+		return winner;
+	}
+
+	// if we reached here nobody has won yet
+
+	// if any empty box on tttData.gameData then play on
+	for (int i = 0; i <= 2; i++)
+	{
+		for (int j = 0; j <= 2; j++)
+		{
+			if (tttData.gameData[i][j] == 0)
+			{
+				winner = 0;
+				return winner;
+			}
+		}
+	}
 }
